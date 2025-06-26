aa7894c3f056c8878a04fa238edd28f6d4a73b16
gbeminiyi <rebel.game09@gmail.com>
2025-06-26 11:16:18 +0100

    java

---
+++ src/calo/Calo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calo;
+
+/**
+ *
+ * @author theon
+ */
+public class Calo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         callo pen = new callo();
+         pen.setVisible(true);
+    }
+    
+}
